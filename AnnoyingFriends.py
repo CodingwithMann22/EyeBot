076fbad9246c1d59977a61111c95fe0d06481408
Mann Parekh <164914030+CodingwithMann22@users.noreply.github.com>
2025-03-10 14:49:03 +0530

    Delete AnnoyingFriends.py

--- AnnoyingFriends.py
+++
@@ -1,9 +0,0 @@
-import pyautogui as pg
-import random
-import time
-time.sleep(10)
-animals = ["dog", "donkey", "bear", "monkey", "bully"]
-x = random.choice(animals)
-for i in range(50):
-    pg.write(f"You are a {x}")
-    pg.press("Enter")                   
